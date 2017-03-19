ed8017c654e523363bd6107c489ffde1949971de
Hlappa <aleksi.holappa@hotmail.com>
2017-03-19 18:01:04 +0200

    Login creds

--- app/components/accountService.js
+++ app/components/accountService.js
@@ -2,28 +2,5 @@ app.factory('accountService', function() {
 
     return {
 
-        fbLogin: function () {
-            FB.login(function (response) {
-                if (response.authResponse) {
-                    console.log('Welcome!  Fetching your information.... ');
-                    FB.api('/me', function (response) {
-                        console.log('Good to see you, ' + response.name + '.');
-                    });
-                } else {
-                    console.log('User cancelled login or did not fully authorize.');
-                }
-            })
-        },
-
-        fbInit: function () {
-            FB.init({
-                appId: '1648610375442724',
-                status: true,
-                cookie: true,
-                xfbml: true,
-                version: 'v2.8'
-            });
-
-        }
 
     }});
