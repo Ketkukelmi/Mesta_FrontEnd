ca48a83c2355400a901686cc882a5dae406cb4c6
Hlappa <aleksi.holappa@hotmail.com>
2017-03-19 17:36:32 +0200

    Account graphics
    Button grpahics not working, working on it

--- app/components/account/accountCtrl.js
+++ app/components/account/accountCtrl.js
@@ -1,7 +1,55 @@
 app.controller('accountCtrl', function ($scope, $window, accountService) {
 
+    $window.fbAsyncInit = function() {
+        // Executed when the SDK is loaded
+        FB.init({
+
+            /*
+             The app id of the web app;
+             To register a new app visit Facebook App Dashboard
+             ( https://developers.facebook.com/apps/ )
+             */
+            appId: '1648610375442724',
+
+            /*
+             Set if you want to check the authentication status
+             at the start up of the app
+             */
+            status: true,
+
+            /*
+             Enable cookies to allow the server to access
+             the session
+             */
+            cookie: true,
+
+            /* Parse XFBML */
+            xfbml: true
+        });
+
+        sAuth.watchAuthenticationStatusChange();
+
+    };
+
+    (function(d){
+        // load the Facebook javascript SDK
+
+        var js,
+            id = 'facebook-jssdk',
+            ref = d.getElementsByTagName('script')[0];
+
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement('script');
+        js.id = id;
+        js.async = true;
+        js.src = "//connect.facebook.net/en_US/all.js";
+        ref.parentNode.insertBefore(js, ref);
+    }(document));
+
     function Login() {
-        FB.api(function (response) {
+        FB.login(function (response) {
 
         })
     }
