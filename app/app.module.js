e860fff2a073e6a345e905148fa2c93bc34c2f9b
Hlappa <aleksi.holappa@hotmail.com>
2017-03-27 11:29:07 +0300

    Deleted the crossdomain

--- app/app.module.js
+++ app/app.module.js
@@ -1,6 +1,2 @@
 /* Define AngularJS application and its dependicies */
 var app = angular.module("app", []);
-app.config(function($httpProvider) {
-    //Enable cross domain calls
-    $httpProvider.defaults.useXDomain = true;
-});
