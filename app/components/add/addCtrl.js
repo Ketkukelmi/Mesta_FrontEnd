c607210f8d37f9fc924fb1c2ec6b03fd23440829
Thom van den Akker <thvda@tele2.nl>
2017-04-12 10:11:01 +0200

    Created file add menu image upload

--- app/components/add/addCtrl.js
+++ app/components/add/addCtrl.js
@@ -1,4 +1,3 @@
-
 app.controller('addCtrl', function ($scope, $rootScope, mapService, postService) {
     $scope.latitude = 0;
     $scope.longitude = 0;
@@ -10,6 +9,28 @@ app.controller('addCtrl', function ($scope, $rootScope, mapService, postService)
     $scope.addLocation = function () {
         postService.addLocation($scope.latitude, $scope.longitude, $scope.name, $scope.description, $scope.tags,$scope.categories);
     };
+
+    $scope.uploadFile = function(elem) {
+        var files = elem.files;
+
+        for (var i = 0, f; f = files[i]; i++) {
+            if (!f.type.match('image.*')) {
+                continue;
+            }
+        }
+
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+            return function(e) {
+                document.getElementById("imgPreview").src = e.target.result;
+            };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+    }
 
     $('map-view').on('click', function () {
         $scope.$apply(function () {
